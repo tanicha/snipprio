c62189706db706d97bbc41ea15d5f0ad2dc3446d
Tani Cha <tanijcha@gmail.com>
2023-07-06 15:15:14 -0700

    added User model

---
+++ src/main/java/com/multiversebackend/snipprio/model/User.java
@@ -0,0 +1,26 @@
+package com.multiversebackend.snipprio.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
